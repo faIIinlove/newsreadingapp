a6b69dde8ea802293239b6187b81c0983e277e56
faIIinlove <106572802+faIIinlove@users.noreply.github.com>
2023-09-27 12:07:23 +0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet
+} from 'react-native';
+
+import AppNavigation from './src/navigation/AppNavigation';
+import { UserProvider } from './src/contexts/UserContext';
+import { NewsProvider } from './src/contexts/NewsContext';
+
+function App(): JSX.Element {
+  return (
+      <SafeAreaView style={styles.body}>
+        <UserProvider>
+          <NewsProvider>
+            <AppNavigation />
+          </NewsProvider>
+        </UserProvider>
+      </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    // backgroundColor:'red',
+  }
+});
+
+export default App;
